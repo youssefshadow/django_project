40c9487bb610a8aa4bb603e3ea990be7f40d0ec4
youssefshadow <youssef20ben@gmail.com>
2024-02-12 19:00:32 +0100

    MAJ front items

--- core/static/javascript/script.js
+++ core/static/javascript/script.js
@@ -1,8 +1,11 @@
 console.log('Le fichier JavaScript est chargé!');
- // JavaScript to toggle the mobile menu visibility
- document.getElementById('mobile-menu-button').addEventListener('click', function () {
-    document.getElementById('mobile-menu').classList.toggle('hidden');
-});
+ 
+let mobileMenuButton = document.getElementById('mobile-menu-button');
+let mobileMenu = document.getElementById('mobile-menu');
 
+mobileMenuButton.addEventListener('click', function () {
+    mobileMenu.classList.toggle('hidden');
+    mobileMenuButton.classList.toggle('menu-closed');
+});
 
 
